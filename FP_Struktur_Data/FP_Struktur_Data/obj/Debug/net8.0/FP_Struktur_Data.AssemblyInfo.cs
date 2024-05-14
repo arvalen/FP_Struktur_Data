b2c37bb7f3320af29e5b7bb6c7d561d5318e5c0a
arvalen <aruanvalentino@gmail.com>
2024-05-14 21:34:32 +0700

    FP SD

--- FP_Struktur_Data/FP_Struktur_Data/obj/Debug/net8.0/FP_Struktur_Data.AssemblyInfo.cs
+++ FP_Struktur_Data/FP_Struktur_Data/obj/Debug/net8.0/FP_Struktur_Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FP_Struktur_Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2e777f0a07a32deac01b31d9f79a7fb5c93d888")]
 [assembly: System.Reflection.AssemblyProductAttribute("FP_Struktur_Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FP_Struktur_Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
