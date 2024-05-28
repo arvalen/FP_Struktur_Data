bd34bc0f935ff62f324f74b30837d633a841ed5e
arvalen <aruanvalentino@gmail.com>
2024-05-29 00:20:50 +0700

    SIMQ

--- FP_Struktur_Data/FP_Struktur_Data/obj/Debug/net8.0/FP_Struktur_Data.AssemblyInfo.cs
+++ FP_Struktur_Data/FP_Struktur_Data/obj/Debug/net8.0/FP_Struktur_Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FP_Struktur_Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51adf82217f3aef48667e225096630f299d06c1d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+635f2c62f2d5e70c7708a893975bf8ba31f36850")]
 [assembly: System.Reflection.AssemblyProductAttribute("FP_Struktur_Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FP_Struktur_Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
